62061efee7b4a50a71e5b99ae67e46db960865a8
BlazeWasHere <blaze@dorime.org>
2021-09-12 12:27:56 +0000

    Patch our MockHTTPClient in nertivia objects (only works in master branch)

--- npytest/factory.py
+++ npytest/factory.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from typing import List
+
 import nertivia
 
 from .http import MockHTTPClient
@@ -10,62 +11,13 @@ _messages: List[nertivia.Message] = []
 
 
 class MockMessage(nertivia.Message):
-    def __init__(self, message, **kwargs):
-        """
-        Set different variables that'll be used later on for ease of access
-        """
-        # If a message object is passed then use its information to create this Message object
-        # Otherwise create a new one from nothing
-        # rewrite `nertivia.Channel` to use `MockHTTPClient`
-        self.http = MockHTTPClient()
-
-        if "message" in message:
-            self.id: int = message['message']['messageID']
-            self.content: str = message['message']['message']
-            self.channel: nertivia.Channel = self.http.get_channel(
-                message["message"]["channelID"])
-            self.author: str = message['message']['creator'][
-                'username'] + '@' + message['message']['creator']['tag']
-
-        else:
-            self.id: int = message['messageID']
-            self.content: str = message['message']
-            self.channel: nertivia.Channel = self.http.get_channel(
-                message["channelID"])
-            self.author: str = message['creator']['username'] + '@' + message[
-                'creator']['tag']
-
-        self.server: nertivia.Server = self.channel.server
+    async def send(self, message: str) -> None:
+        mes = make_message(message)
+        _messages.append(mes)
 
 
 class MockChannel(nertivia.Channel):
-    def __init__(self, channel, **kwargs):
-        # rewrite `nertivia.Channel` to use `MockHTTPClient`
-        self.http = MockHTTPClient()
-
-        # Set all of the properties of the channel in a self.x blob, consider cleaning up if possible
-        # If a Channel object is provided in the arguments, then just extract all of its information
-        if "channel" in channel:
-            self.id = channel["channel"]["channelID"]
-            self.name = channel["channel"]["name"]
-            self.status = channel["channel"]["status"]
-            self.name = channel["channel"]["name"]
-            self.server = self.http.get_server(  # type: ignore
-                channel["channel"]["server_id"])
-            self.last_messaged = channel["channel"]["timestamp"]
-            self._channel = channel["channel"]
-
-        # If it is a new Channel object, then we have less information to set and use what was provided
-        else:
-            self.id = channel["channelID"]
-            self.name = channel["name"]
-            self.server = self.http.get_server(  # type: ignore
-                channel["server_id"])
-            if "timestamp" in channel:
-                self.last_messaged = channel["timestamp"]
-            self._channel = channel
-
-    async def send(self, message: str):
+    async def send(self, message: str) -> None:
         mes = make_message(message)
         _messages.append(mes)
 
@@ -88,7 +40,7 @@ def make_message(message: str) -> nertivia.Message:
         }
     }
 
-    return MockMessage(data)
+    return MockMessage(data, http=MockHTTPClient())
 
 
 def make_channel(channel_id: int) -> nertivia.Channel:
@@ -98,7 +50,7 @@ def make_channel(channel_id: int) -> nertivia.Channel:
         'server_id': '0000000000000000000'
     }
 
-    return MockChannel(data)
+    return MockChannel(data, http=MockHTTPClient())
 
 
 def make_server(server_id: int) -> nertivia.Server:
