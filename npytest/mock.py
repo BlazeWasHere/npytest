d6ebb8c2a164986ab81453ccc4f779382ed85b48
BlazeWasHere <blaze@dorime.org>
2021-09-13 17:50:54 +0000

    docstrings

--- npytest/mock.py
+++ npytest/mock.py
@@ -12,6 +12,16 @@ from .http import MockHTTPClient
 
 
 def configure(bot: nertivia.Bot):
+    """
+    The "init" command of npytest. Call it to setup your nertivia bot.
+
+    Args:
+        bot (nertivia.Bot): Object of the nertivia.py bot.
+
+    Raises:
+        TypeError: If `bot` is not an instance of `nertivia.Bot`.
+    """
+
     if not isinstance(bot, nertivia.Bot):
         raise TypeError("bot must be an instance of nertivia.Bot")
 
@@ -26,6 +36,26 @@ def configure(bot: nertivia.Bot):
 
 
 def message(msg: str) -> List[nertivia.Message]:
+    """
+    Mock a message in a mock nertivia channel.
+
+    Args:
+        msg (str): The mock message content.
+
+    Raises:
+        TypeError: If there is no `on_message` callback set.
+
+    Returns:
+        List[nertivia.Message]: List of mock `nertivia.Message`s sent by the bot.
+            The first message of the array is the message sent by this function.
+
+    Examples:
+        >>> message('test')
+        [<id=9993649525527305728 content='test' channel=<<id=0000000000000000000 name='npytest' server=<<id=0000000000000000000 name='npytest' default_channel=<0000000000000000000>>>>> server=<<id=0000000000000000000 name='npytest' default_channel=<0000000000000000000>>> author='npytest@0000'>]
+        >>> message('!ping')[0]
+        <id=6986161856568744966 content='pong' channel=<<id=0000000000000000000 name='npytest' server=<<id=0000000000000000000 name='npytest' default_channel=<0000000000000000000>>>>> server=<<id=0000000000000000000 name='npytest' default_channel=<0000000000000000000>>> author='npytest@0000'>
+    """
+
     obj = make_message(msg)
     # Clear previous message states.
     _message_ids.clear()
@@ -43,6 +73,20 @@ def message(msg: str) -> List[nertivia.Message]:
 
 
 def edit(msg: nertivia.Message, content: str) -> nertivia.Message:
+    """
+    Edit a mock nertivia.Message and change it's content to `content`.
+
+    Note:
+        Internally, if `msg` does not exist, None is returned.
+
+    Args:
+        msg (nertivia.Message): The nertivia.Message object to change.
+        content (str): The content to change `msg.content` to.
+
+    Returns:
+        nertivia.Message: Exact same as `msg`.
+    """
+
     # Clear previous message states.
     _message_ids.clear()
 
